4292e52f1ca9913bcd1c87ea229921a45577af19
Sh4wn-Fr4nk1in <74861140+Sh4wn-Fr4nk1in@users.noreply.github.com>
2022-02-28 21:54:25 +0800

    Add COD Template and init script

---
+++ Computer Organization Principle/init.ps1
@@ -0,0 +1,12 @@
+# Copyright (c) 2022 Fr4nk1in-USTC
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+$hwnum = Read-Host "Please input the number of the homework: "
+mkdir ./$hwnum
+cp ./template/homework.cls ./$hwnum/homework.cls
+cp ./template/template.tex ./$hwnum/$hwnum.tex
+$file = Get-Content -Path ./$hwnum/$hwnum.tex
+$file[6] = $file[6].replace("0", $hwnum)
+Set-Content -Path ./$hwnum/$hwnum.tex -Value $file
