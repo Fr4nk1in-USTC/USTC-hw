4213d9e664a8cc463286b99037ee92e04e642a4a
Sh4wn-Fr4nk1in <74861140+Sh4wn-Fr4nk1in@users.noreply.github.com>
2022-02-28 21:57:44 +0800

    Update init script in COD to meet requirement.

--- Computer Organization Principle/init.ps1
+++ Computer Organization Principle/init.ps1
@@ -6,7 +6,7 @@
 $hwnum = Read-Host "Please input the number of the homework: "
 mkdir ./$hwnum
 cp ./template/homework.cls ./$hwnum/homework.cls
-cp ./template/template.tex ./$hwnum/$hwnum.tex
-$file = Get-Content -Path ./$hwnum/$hwnum.tex
+cp ./template/template.tex ./$hwnum/PB20000051_傅申_HW0$hwnum.tex
+$file = Get-Content -Path ./$hwnum/PB20000051_傅申_HW0$hwnum.tex
 $file[6] = $file[6].replace("0", $hwnum)
-Set-Content -Path ./$hwnum/$hwnum.tex -Value $file
+Set-Content -Path ./$hwnum/PB20000051_傅申_HW0$hwnum.tex -Value $file
